ec188b196de5a08c76f313eb7c62e7e69afa0338
psergeyv <psergeyv@users.noreply.github.com>
2020-10-16 08:53:04 +0700

    user create 1

--- app/foodgram/settings.py
+++ app/foodgram/settings.py
@@ -73,14 +73,15 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),       
-    }
+    'default': {'DATABASE_URL'='psql://yatube_user:#58fL*dS3@127.0.0.1:5432/yatube'}
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': os.environ.get('DB_NAME'),
+    #    'USER': os.environ.get('POSTGRES_USER'),
+    #    'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+    #    'HOST': os.environ.get('DB_HOST'),
+    #    'PORT': os.environ.get('DB_PORT'),       
+    #}
 }
 
 
