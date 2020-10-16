f7a1b2ac073b3d4ee3ca27c0fdcfff0761a3e0bf
psergeyv <psergeyv@users.noreply.github.com>
2020-10-16 14:29:37 +0700

    docker cash

--- app/foodgram/settings.py
+++ app/foodgram/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {'DATABASE_URL'='psql://foodgram:foodgram@127.0.0.1:5432/foodgram'}
+    'default': {'DATABASE_URL':'psql://foodgram:foodgram@127.0.0.1:5432/foodgram'}
     #'default': {
     #    'ENGINE': 'django.db.backends.postgresql',
     #    'NAME': os.environ.get('DB_NAME'),
